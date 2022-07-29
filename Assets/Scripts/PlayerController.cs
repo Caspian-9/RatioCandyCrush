6181cabd829951e9579bd91c574dd2e18d195402
Caspian-9 <lakeharmony6854@gmail.com>
2022-07-28 19:43:24 -0700

    added basic 3d platformer part
    and basic B E A N movement and detecting collisions with  C U B E

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+
+    private CharacterController controller;
+
+    public float moveSpeed;
+    public float gravity = Physics.gravity.y;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        controller = GetComponent<CharacterController>();
+        moveSpeed = 10f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 move = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+        controller.Move(moveSpeed * Time.deltaTime * move);
+    }
+
+    private void OnTriggerEnter(Collider other) {
+
+        // other is the collider of the gameobject the bean is hitting
+        if (other.gameObject.tag == "Level") {
+
+            Debug.Log("bonk");
+        }
+    }
+}
