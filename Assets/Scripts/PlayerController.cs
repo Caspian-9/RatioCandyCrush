e058e770f8dfcb39f4704594c3e239d06545082c
Caspian-9 <lakeharmony6854@gmail.com>
2022-08-12 02:14:21 -0700

    jump works, also you cant clip through pink bean and boat anymore

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -6,24 +6,48 @@ public class PlayerController : MonoBehaviour
 {
 
     private CharacterController controller;
+    private Rigidbody RB;
 
     public Manager manager;
 
     public float moveSpeed;
-    public float gravity = Physics.gravity.y;
+    public float gravity = Physics.gravity.y * 5;
+    public float jumpForce;
 
     // Start is called before the first frame update
     void Start()
     {
         controller = GetComponent<CharacterController>();
+        RB = GetComponent<Rigidbody>();
         moveSpeed = 10f;
+        jumpForce = 10f;
     }
 
     // Update is called once per frame
     void Update()
     {
-        Vector3 move = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
-        controller.Move(moveSpeed * Time.deltaTime * move);
+        RB.velocity = new Vector3(Input.GetAxis("Horizontal") * moveSpeed, RB.velocity.y, Input.GetAxis("Vertical") * moveSpeed);
+
+        if (Input.GetButtonDown("Jump"))
+        {
+            RB.velocity = new Vector3(RB.velocity.x, jumpForce, RB.velocity.z);
+        }
+
+        if (gameObject.transform.position.y < -20)
+        {
+            gameObject.transform.position = new Vector3(0, 1, 0);
+        }
+
+
+        // this uses CharacterController
+
+        //Vector3 move = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+        //controller.Move(moveSpeed * Time.deltaTime * move);
+
+        //if (gameObject.transform.position.y < -10)
+        //{
+        //    gameObject.transform.position = new Vector3(0, 1, 0);
+        //}
     }
 
     private void OnTriggerEnter(Collider other) {
